e2daf3e6efae08b1972f83d2953dc652e100f383
Ashutoshkumarsinghgit <105078174+Ashutoshkumarsinghgit@users.noreply.github.com>
2022-05-07 02:47:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import date
 import streamlit as st
 from streamlit.components.v1 import iframe
 
-st.set_page_config(layout="centered", page_icon="🎓", page_title="Meenakshi.com")
+st.set_page_config(layout="centered", page_icon="🎓", page_title="Meenakshi")
 st.title("🎓 Free Certificates")
 
 st.write(
